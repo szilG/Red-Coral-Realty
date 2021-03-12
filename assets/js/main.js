8adf127939c405c3b885281a603a050bf5902b6a
szilG <gombiszilvi@gmail.com>
2021-03-12 11:31:13 +0000

    add navbar animation when load and add property section animation when scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,29 @@
+/*--Scroll on move---*/
+window.sr = ScrollReveal();
+sr.reveal(".navbar" , {
+  duration: 2000,
+  origin: "bottom"
+});
+sr.reveal(".move-from-left" , {
+  duration: 2000,
+  origin: "left",
+  distance: "500px",
+  viewFactor: 0.2
+});
+sr.reveal(".move-from-right" , {
+  duration: 2000,
+  origin: "right",
+  distance: "500px",
+  viewFactor: 0.2
+});
+sr.reveal(".box" , {
+  duration: 2000,
+  origin: "bottom",
+  distance: "300px",
+  viewFactor: 0.2
+});
+
+
 //=====================Back to Top button============================//
 const backToTopButton = document.querySelector("#back-to-top-btn");
 
