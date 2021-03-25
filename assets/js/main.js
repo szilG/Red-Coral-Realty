66aead42dd1fcac87c0ea2ceb51603f308988c13
szilG <gombiszilvi@gmail.com>
2021-03-25 14:40:41 +0000

    add new page add style and image for it

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,3 @@
-//=============Start ScrollReveal --Scroll on move-- ======//
-window.sr = ScrollReveal();
-sr.reveal(".navbar" , {
-  duration: 2000,
-  origin: "bottom"
-});
-sr.reveal(".move-from-left" , {
-  duration: 2000,
-  origin: "left",
-  distance: "500px",
-  viewFactor: 0.2
-});
-sr.reveal(".move-from-right" , {
-  duration: 2000,
-  origin: "right",
-  distance: "500px",
-  viewFactor: 0.2
-});
-sr.reveal(".box" , {
-  duration: 2000,
-  origin: "bottom",
-  distance: "300px",
-  viewFactor: 0.2
-});
-//=============End ScrollReveal --Scroll on move-- ======//
 
 //=====================Start Back to Top button============================//
 const backToTopButton = document.querySelector("#back-to-top-btn");
